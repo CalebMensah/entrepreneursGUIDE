81184c35d1123b13f511762f973f0c96edd97cab
Caleb <mensahkhalib28@gmail.com>
2024-08-28 01:59:46 +0000

    redirect user to home after sign up

--- public/js/script.js
+++ public/js/script.js
@@ -4,3 +4,34 @@ const navLinks = document.querySelector('.nav-links');
 menuToggle.addEventListener('click', ()=> {
     navLinks.classList.toggle('active')
 })
+
+
+// redirecting the user to homepage after signup
+document.addEventListener('DOMContentLoaded', function () {
+    const signupForm = document.getElementById('signup-form');
+
+    signupForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+        const formData = new FormData(signupForm);
+        const action = signupForm.getAttribute("action");
+        const method = signupForm.getAttribute("method");
+        fetch(action, {
+            method: method,
+            body: formData,
+            headers: {
+                'Accept': 'application/json'
+            }
+        })
+        .then (response => {
+            if (response.ok) {
+                window.location.href = '/';
+            } else {
+                alert("There was a problem with the sign up")
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert("There was a problem with the sign up")
+        });
+    })
+})
